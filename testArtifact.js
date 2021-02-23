251dd1f4a582fc899f706cc0f9964a45568db267
Vasyl Herman PC W10 <vasil.german@gmail.com>
2021-02-23 21:15:46 +0200

    try to use ensureValueSets...

--- testArtifact.js
+++ testArtifact.js
@@ -19,10 +19,14 @@ const fhirBundle2 = JSON.parse(fs.readFileSync(
     path.join(__dirname, 'r4', 'bundles', 'Luna60.json'),'utf8'));
 
 const codeService = new cqlvsac.CodeService(path
-    .join(__dirname, 'r4', 'valueset'),true);
+    .join(__dirname, 'r4', 'cql', 'testArtifact', 'vsac_cache'),true);
 
 // const library = new cql.Library(elmFile, new cql.Repository(libraries));
 const library = new cql.Library(elmFile);
+
+codeService.ensureValueSetsWithAPIKey(library, 'f52a90ad-4d3b-4955-ba5c-635264536b53', true)
+    .then(() => console.log('Value Sets are downloaded'))
+    .catch((err) => console.log('Error downloading value sets', err))
 
 const executor = new cql.Executor(library, codeService);
 // const executor = new cql.Executor(library);

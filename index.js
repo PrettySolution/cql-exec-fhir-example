fa2191d78423640c82ad78204483084c76b407c3
Vasyl Herman PC W10 <vasil.german@gmail.com>
2021-02-25 13:08:15 +0200

    add more comments

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const bundlesPath = path.join(__dirname, 'r4', 'bundles');
 const measurePath = path.join(__dirname, 'r4', 'cql', process.env.MEASURE);
 
 function bundlesBuilder(bPath){
-    // bundles
+    // Load all bundles that have been found in ./r4/bundles folder
     let b = [];
     for (const fileName of fs.readdirSync(bPath)) {
         const file = path.join(bPath, fileName);
@@ -22,6 +22,8 @@ function bundlesBuilder(bPath){
         const json = JSON.parse(fs.readFileSync(file, 'utf8'));
         b.push(json)
     }
+
+    // Return bundles array
     return b;
 }
 
@@ -30,12 +32,14 @@ function libraryBuilder(mPath) {
     const mainCql = JSON.parse(fs.readFileSync(
         path.join(mPath, `main.json`),'utf8'));
 
+    // Load included libraries from mainCql
     const includes = mainCql.library.includes && mainCql.library.includes.def || [];
     let includedLibs = {};
     includes.forEach((l)=>{
         includedLibs[l.localIdentifier] = JSON.parse(fs.readFileSync(path.join(mPath, `${l.path}-${l.version}.json`)));
     });
 
+    // Return Library
     return new cql.Library(mainCql, new cql.Repository(includedLibs));
 }
 
@@ -45,12 +49,15 @@ const bundles = bundlesBuilder(bundlesPath);
 const codeService = new cqlvsac.CodeService(
     path.join(measurePath, 'vsac_cache'),true);
 
+// Ensure that all the value sets from included libraries are present in cache folder
+// if not - load from Value Set Authority Center
 codeService.ensureValueSetsInLibraryWithAPIKey(library, true , undefined, true)
     .then(() => {
-        console.log('ensureValueSetsInLibraryWithAPIKey');
+        console.log('All the value sets in place');
         const executor = new cql.Executor(library, codeService, undefined);
         const patientSource = cqlfhir.PatientSource.FHIRv401();
         patientSource.loadBundles(bundles);
+        // execution:
         const result = executor.exec(patientSource);
         console.log(result);
         console.log('Done');
